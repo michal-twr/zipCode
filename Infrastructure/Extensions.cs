c7432add708f9c2ee66b352398db1509396fb2c9
Michal Matuszewski <Michal.Matuszewski@stepstone.com>
2023-06-30 11:22:10 +0200

    Using Polly to have 3 retries for zippopotam Client

--- Infrastructure/Extensions.cs
+++ Infrastructure/Extensions.cs
@@ -1,13 +1,27 @@
 using Core;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace Infrastructure;
 
 public static class Extensions
 {
+
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        services.AddHttpClient<IZipApiClient, ZippopotamClient>();
+        services.AddHttpClient<IZipApiClient, ZippopotamClient>()
+            .SetHandlerLifetime((TimeSpan.FromMinutes(2)))
+            .AddPolicyHandler(GetRetryPolicy());
+            
         return services;
     }
+
+    private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .WaitAndRetryAsync(3, retry => TimeSpan.FromMilliseconds(10));
+    }
+    
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # zipCode
 
 Fun and play with ASP .Net
+
+
+## gitignore file source
+https://gist.github.com/indyfromoz/4109296
